af8109e35765b885c521b8fe92458009ed5af151
Kamil Monicz <kamil.monicz@outlook.com>
2019-01-22 15:57:00 +0100

    NuGet preparations

--- 2Captcha.Test/Program.cs
+++ 2Captcha.Test/Program.cs
@@ -7,25 +7,32 @@ namespace _2Captcha.Test
     {
         static void Main(string[] args)
         {
-            var twoCaptcha = new TwoCaptcha("11111111111111111111111111111111");
+            var twoCaptcha = new TwoCaptcha(" ## YOUR API KEY ## ");
 
-            var image1 = twoCaptcha.SolveImage(new FileStream("captcha.png", FileMode.Open));
-            var image2 = twoCaptcha.SolveImage("data:image/png;base64,iVBORw0KGgo...");
+            // Solve image captcha
+            var image1 = twoCaptcha.SolveImage(new FileStream("captcha.png", FileMode.Open)).Result;
+            var image2 = twoCaptcha.SolveImage("data:image/png;base64,iVBORw0KGgo...").Result;
 
-            var question = twoCaptcha.SolveQuestion("1 + 3 = ?");
-            
-            var recaptcha = twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "http://example.com");
-            var recaptchaInvisible = twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "http://example.com", true);
-            
-            var click1 = twoCaptcha.SolveClickCaptcha(new FileStream("captcha.png", FileMode.Open), "Click on ghosts");
-            var click2 = twoCaptcha.SolveClickCaptcha("data:image/png;base64,iVBORw0KGgo...", "Click on ghosts");
+            // Solve text captcha
+            var question = twoCaptcha.SolveQuestion("1 + 3 = ?").Result;
 
-            var rotate = twoCaptcha.SolveRotateCaptcha(new Stream[] {new FileStream("captcha.png", FileMode.Open)}, "40");
+            // Solve ReCaptchaV2
+            var recaptcha = twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com").Result;
+            var recaptchaInvisible = twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com", true).Result;
 
-            var fun = twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "http://example.com");
-            var funNoJavaScript = twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "http://example.com", true);
+            // Solve ClickCaptcha
+            var click1 = twoCaptcha.SolveClickCaptcha(new FileStream("captcha.png", FileMode.Open), "Click on ghosts").Result;
+            var click2 = twoCaptcha.SolveClickCaptcha("data:image/png;base64,iVBORw0KGgo...", "Click on ghosts").Result;
 
-            var key = twoCaptcha.SolveKeyCaptcha("USER_ID", "SESSION_ID", "WEB_SIGN_1", "WEB_SIGN_2", "http://example.com");
+            // Solve RotateCaptcha
+            var rotate = twoCaptcha.SolveRotateCaptcha(new Stream[] {new FileStream("captcha.png", FileMode.Open)}, "40").Result;
+
+            // Solve FunCaptcha
+            var fun = twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com").Result;
+            var funNoJavaScript = twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com", true).Result;
+
+            // Solve KeyCaptcha
+            var key = twoCaptcha.SolveKeyCaptcha("USER_ID", "SESSION_ID", "WEB_SIGN_1", "WEB_SIGN_2", "https://example.com").Result;
 
             Debugger.Break();
         }
