7091fb1e7bfc9eb30893af237fd49d32ceab9832
Kamil Monicz <kamil.monicz@outlook.com>
2019-02-26 18:58:47 +0100

    Updated test project #2

--- 2Captcha.Test/Program.cs
+++ 2Captcha.Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace _2Captcha.Test
 {
@@ -7,35 +8,40 @@ namespace _2Captcha.Test
     {
         static void Main(string[] args)
         {
+            Foo().GetAwaiter().GetResult();
+        }
+
+        static async Task Foo()
+        {
             var twoCaptcha = new TwoCaptcha(" ## YOUR API KEY ## ");
 
             // Get current balance
-            var balance = twoCaptcha.GetBalance().Result;
+            var balance = await twoCaptcha.GetBalance();
 
             // Solve image captcha
-            var image1 = twoCaptcha.SolveImage(new FileStream("captcha.png", FileMode.Open)).Result;
-            var image2 = twoCaptcha.SolveImage("data:image/png;base64,iVBORw0KGgo...").Result;
+            var image1 = await twoCaptcha.SolveImage(new FileStream("captcha.png", FileMode.Open));
+            var image2 = await twoCaptcha.SolveImage("data:image/png;base64,iVBORw0KGgo...");
 
             // Solve text captcha
-            var question = twoCaptcha.SolveQuestion("1 + 3 = ?").Result;
+            var question = await twoCaptcha.SolveQuestion("1 + 3 = ?");
 
             // Solve ReCaptchaV2
-            var recaptcha = twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com").Result;
-            var recaptchaInvisible = twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com", true).Result;
+            var recaptcha = await twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com");
+            var recaptchaInvisible = await twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com", true);
 
             // Solve ClickCaptcha
-            var click1 = twoCaptcha.SolveClickCaptcha(new FileStream("captcha.png", FileMode.Open), "Click on ghosts").Result;
-            var click2 = twoCaptcha.SolveClickCaptcha("data:image/png;base64,iVBORw0KGgo...", "Click on ghosts").Result;
+            var click1 = await twoCaptcha.SolveClickCaptcha(new FileStream("captcha.png", FileMode.Open), "Click on ghosts");
+            var click2 = await twoCaptcha.SolveClickCaptcha("data:image/png;base64,iVBORw0KGgo...", "Click on ghosts");
 
             // Solve RotateCaptcha
-            var rotate = twoCaptcha.SolveRotateCaptcha(new Stream[] {new FileStream("captcha.png", FileMode.Open)}, "40").Result;
+            var rotate = await twoCaptcha.SolveRotateCaptcha(new Stream[] {new FileStream("captcha.png", FileMode.Open)}, "40");
 
             // Solve FunCaptcha
-            var fun = twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com").Result;
-            var funNoJavaScript = twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com", true).Result;
+            var fun = await twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com");
+            var funNoJavaScript = await twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com", true);
 
             // Solve KeyCaptcha
-            var key = twoCaptcha.SolveKeyCaptcha("USER_ID", "SESSION_ID", "WEB_SIGN_1", "WEB_SIGN_2", "https://example.com").Result;
+            var key = await twoCaptcha.SolveKeyCaptcha("USER_ID", "SESSION_ID", "WEB_SIGN_1", "WEB_SIGN_2", "https://example.com");
 
             Debugger.Break();
         }
