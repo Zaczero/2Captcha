47b39ae381b9433f53889bf5b6adb99c9994eed9
Kamil Monicz <kamil@monicz.pl>
2020-03-26 15:28:31 +0100

    Updated sandbox code

--- 2Captcha.Sandbox/Program.cs
+++ 2Captcha.Sandbox/Program.cs
@@ -1,10 +1,11 @@
 ﻿using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace _2Captcha.Test
 {
-	class Program
+    class Program
     {
         static void Main(string[] args)
         {
@@ -13,38 +14,43 @@ namespace _2Captcha.Test
 
         static async Task Foo()
         {
-            var twoCaptcha = new TwoCaptcha(" ## YOUR API KEY ## ");
+            var captcha = new _2Captcha(" ## YOUR API KEY ## ");
+            // .. additionally you can pass your own httpClient class
+            var captchaWithHttpClient = new _2Captcha(" ## YOUR API KEY ## ", new HttpClient());
 
             // Get current balance
-            var balance = await twoCaptcha.GetBalance();
+            var balance = await captcha.GetBalance();
 
             // Solve image captcha
-            var image1 = await twoCaptcha.SolveImage(new FileStream("captcha.png", FileMode.Open));
-            var image2 = await twoCaptcha.SolveImage("data:image/png;base64,iVBORw0KGgo...");
+            var image1 = await captcha.SolveImage(new FileStream("captcha.png", FileMode.Open));
+            var image2 = await captcha.SolveImage("data:image/png;base64,iVBORw0KGgo...");
 
             // Solve text captcha
-            var question = await twoCaptcha.SolveQuestion("1 + 3 = ?");
+            var question = await captcha.SolveQuestion("1 + 3 = ?");
+
+            // Solve hCaptcha
+            var hcaptcha = await captcha.SolveHCaptcha("HCAPTCHA_SITE_KEY", "https://example.com");
 
             // Solve ReCaptchaV2
-            var recaptcha = await twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com");
-            var recaptchaInvisible = await twoCaptcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com", true);
+            var recaptcha = await captcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com");
+            var recaptchaInvisible = await captcha.SolveReCaptchaV2("GOOGLE_SITE_KEY", "https://example.com", true);
 
             // Solve ReCaptchaV3
-            var recaptcha3 = await twoCaptcha.SolveReCaptchaV3("GOOGLE_SITE_KEY", "https://example.com", "ACTION", 0.4);
+            var recaptcha3 = await captcha.SolveReCaptchaV3("GOOGLE_SITE_KEY", "https://example.com", "ACTION", 0.4);
 
-			// Solve ClickCaptcha
-			var click1 = await twoCaptcha.SolveClickCaptcha(new FileStream("captcha.png", FileMode.Open), "Click on ghosts");
-            var click2 = await twoCaptcha.SolveClickCaptcha("data:image/png;base64,iVBORw0KGgo...", "Click on ghosts");
+            // Solve ClickCaptcha
+            var click1 = await captcha.SolveClickCaptcha(new FileStream("captcha.png", FileMode.Open), "Click on ghosts");
+            var click2 = await captcha.SolveClickCaptcha("data:image/png;base64,iVBORw0KGgo...", "Click on ghosts");
 
             // Solve RotateCaptcha
-            var rotate = await twoCaptcha.SolveRotateCaptcha(new Stream[] {new FileStream("captcha.png", FileMode.Open)}, "40");
+            var rotate = await captcha.SolveRotateCaptcha(new Stream[] {new FileStream("captcha.png", FileMode.Open)}, "40");
 
             // Solve FunCaptcha
-            var fun = await twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com");
-            var funNoJavaScript = await twoCaptcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com", true);
+            var fun = await captcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com");
+            var funNoJavaScript = await captcha.SolveFunCaptcha("FUN_CAPTCHA_PUBLIC_KEY", "https://example.com", true);
 
             // Solve KeyCaptcha
-            var key = await twoCaptcha.SolveKeyCaptcha("USER_ID", "SESSION_ID", "WEB_SIGN_1", "WEB_SIGN_2", "https://example.com");
+            var key = await captcha.SolveKeyCaptcha("USER_ID", "SESSION_ID", "WEB_SIGN_1", "WEB_SIGN_2", "https://example.com");
 
             Debugger.Break();
         }
