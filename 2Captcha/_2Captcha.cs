f388290d174b058be5712b814925e5e9eeb55024
Kamil Monicz <kamil@monicz.pl>
2020-11-01 20:26:29 +0100

    Result struct rewrite (support for multiple result types)

--- 2Captcha/_2Captcha.cs
+++ 2Captcha/_2Captcha.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using _2CaptchaAPI.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -13,17 +15,8 @@ using System.Threading.Tasks;
 
 namespace _2CaptchaAPI
 {
-	public class _2Captcha
+	public partial class _2Captcha
 	{
-#if NETSTANDARD2_0
-		[Serializable]
-#endif
-		private struct _2CaptchaResultInternal
-		{
-			public int Status;
-			public string Request;
-		}
-		
 		private string _apiUrl = "https://2captcha.com/";
 
 		private readonly HttpClient _httpClient;
@@ -46,7 +39,7 @@ namespace _2CaptchaAPI
 			_apiUrl = url;
 		}
 
-		public async Task<_2CaptchaResult> GetBalance()
+		public async Task<Result> GetBalance()
 		{
 			var getData = new List<KeyValuePair<string, string>>
 			{
@@ -59,16 +52,11 @@ namespace _2CaptchaAPI
 			var inResponse = await _httpClient.PostAsync(_apiUrl + "res.php", new FormUrlEncodedContent(getData));
 			var inJson = await inResponse.Content.ReadAsStringAsync();
 
-			var @in = JsonConvert.DeserializeObject<_2CaptchaResultInternal>(inJson);
-			if (@in.Status == 0)
-			{
-				return new _2CaptchaResult(false, @in.Request);
-			}
-
-			return new _2CaptchaResult(true, @in.Request);
+			var @in = JsonConvert.DeserializeObject<ResultInternal>(inJson);
+			return new Result(@in.Status == 1, @in.Request);
 		}
-		
-		private async Task<_2CaptchaResult> Solve(string method, int delaySeconds, IEnumerable<KeyValuePair<string, string>> options, MultipartFormDataContent httpContent)
+
+		private async Task<Result> Solve(string method, int delaySeconds, IEnumerable<KeyValuePair<string, string>> options, MultipartFormDataContent httpContent)
 		{
 			httpContent.Add(new StringContent(_apiKey), "key");
 			httpContent.Add(new StringContent(method), "method");
@@ -81,17 +69,15 @@ namespace _2CaptchaAPI
 			var inResponse = await _httpClient.PostAsync(_apiUrl + "in.php", httpContent);
 			var inJson = await inResponse.Content.ReadAsStringAsync();
 
-			var @in = JsonConvert.DeserializeObject<_2CaptchaResultInternal>(inJson);
+			var @in = JsonConvert.DeserializeObject<ResultInternal>(inJson);
 			if (@in.Status == 0)
-			{
-				return new _2CaptchaResult(false, @in.Request);
-			}
-			
+				return new Result(false, @in.Request);
+
 			await Task.Delay(delaySeconds * 1000);
 			return await GetResponse(@in.Request);
 		}
 
-		private async Task<_2CaptchaResult> Solve(string method, int delaySeconds, IEnumerable<KeyValuePair<string, string>> options, params KeyValuePair<string, string>[] args)
+		private async Task<Result> Solve(string method, int delaySeconds, IEnumerable<KeyValuePair<string, string>> options, params KeyValuePair<string, string>[] args)
 		{
 			var postData = new List<KeyValuePair<string, string>>
 			{
@@ -107,41 +93,40 @@ namespace _2CaptchaAPI
 			var inResponse = await _httpClient.PostAsync(_apiUrl + "in.php", new FormUrlEncodedContent(postData));
 			var inJson = await inResponse.Content.ReadAsStringAsync();
 
-			var @in = JsonConvert.DeserializeObject<_2CaptchaResultInternal>(inJson);
+			var @in = JsonConvert.DeserializeObject<ResultInternal>(inJson);
 			if (@in.Status == 0)
-			{
-				return new _2CaptchaResult(false, @in.Request);
-			}
-			
+				return new Result(false, @in.Request);
+
 			await Task.Delay(delaySeconds * 1000);
 			return await GetResponse(@in.Request);
 		}
 
-		private async Task<_2CaptchaResult> GetResponse(string solveId)
+		private async Task<Result> GetResponse(JToken solveIdObject)
 		{
 			var apiKeySafe = Uri.EscapeUriString(_apiKey);
+			var solveId = solveIdObject.ToObject<string>();
 
 			while (true)
 			{
 				var resJson = await _httpClient.GetStringAsync(_apiUrl + $"res.php?key={apiKeySafe}&id={solveId}&action=get&json=1");
 
-				var res = JsonConvert.DeserializeObject<_2CaptchaResultInternal>(resJson);
+				var res = JsonConvert.DeserializeObject<ResultInternal>(resJson);
 				if (res.Status == 0)
 				{
-					if (res.Request == "CAPCHA_NOT_READY")
+					if (res.Request.ToObject<string>() == "CAPCHA_NOT_READY")
 					{
 						await Task.Delay(5 * 1000);
 						continue;
 					}
 
-					return new _2CaptchaResult(false, res.Request);
+					return new Result(false, res.Request);
 				}
 
-				return new _2CaptchaResult(true, res.Request);
+				return new Result(true, res.Request);
 			}
 		}
 
-		public async Task<_2CaptchaResult> SolveImage(Stream imageStream, FileType fileType, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveImage(Stream imageStream, FileType fileType, params KeyValuePair<string, string>[] options)
 		{
 			var httpContent = new MultipartFormDataContent
 			{
@@ -151,7 +136,7 @@ namespace _2CaptchaAPI
 			return await Solve("post", 5, options, httpContent);
 		}
 
-		public async Task<_2CaptchaResult> SolveImage(byte[] imageBytes, FileType fileType, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveImage(byte[] imageBytes, FileType fileType, params KeyValuePair<string, string>[] options)
 		{
 			var imageBase64 = Convert.ToBase64String(imageBytes);
 			var imageData = EnsureValidData(imageBase64, fileType);
@@ -160,7 +145,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("body", imageData));
 		}
 
-		public async Task<_2CaptchaResult> SolveImage(string imageBase64, FileType fileType, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveImage(string imageBase64, FileType fileType, params KeyValuePair<string, string>[] options)
 		{
 			var imageData = EnsureValidData(imageBase64, fileType);
 
@@ -168,13 +153,13 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("body", imageData));
 		}
 
-		public async Task<_2CaptchaResult> SolveQuestion(string question, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveQuestion(string question, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("textcaptcha", 5, options,
 				new KeyValuePair<string, string>("textcaptcha", question));
 		}
 
-		public async Task<_2CaptchaResult> SolveReCaptchaV2(string siteKey, string pageUrl, bool isInvisible = false, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveReCaptchaV2(string siteKey, string pageUrl, bool isInvisible = false, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("userrecaptcha", 10, options,
 				new KeyValuePair<string, string>("googlekey", siteKey),
@@ -182,7 +167,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("invisible", isInvisible ? "1" : "0"));
 		}
 
-		public async Task<_2CaptchaResult> SolveReCaptchaV2(string siteKey, string pageUrl, string proxy, ProxyType proxyType, bool isInvisible = false, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveReCaptchaV2(string siteKey, string pageUrl, string proxy, ProxyType proxyType, bool isInvisible = false, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("userrecaptcha", 10, options,
 				new KeyValuePair<string, string>("googlekey", siteKey),
@@ -192,7 +177,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("invisible", isInvisible ? "1" : "0")) ;
 		}
 
-		public async Task<_2CaptchaResult> SolveReCaptchaV3(string siteKey, string pageUrl, string action = "verify", double minScore = 0.4, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveReCaptchaV3(string siteKey, string pageUrl, string action = "verify", double minScore = 0.4, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("userrecaptcha", 10, options,
 				new KeyValuePair<string, string>("googlekey", siteKey),
@@ -202,7 +187,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("min_score", minScore.ToString(CultureInfo.InvariantCulture)));
 		}
 
-		public async Task<_2CaptchaResult> SolveReCaptchaV3(string siteKey, string pageUrl, string proxy, ProxyType proxyType, string action = "verify", double minScore = 0.4, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveReCaptchaV3(string siteKey, string pageUrl, string proxy, ProxyType proxyType, string action = "verify", double minScore = 0.4, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("userrecaptcha", 10, options,
 				new KeyValuePair<string, string>("googlekey", siteKey),
@@ -214,14 +199,14 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("proxytype", proxyType.GetExtension()));
 		}
 
-		public async Task<_2CaptchaResult> SolveHCaptcha(string siteKey, string pageUrl, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveHCaptcha(string siteKey, string pageUrl, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("hcaptcha", 10, options,
 				new KeyValuePair<string, string>("sitekey", siteKey),
 				new KeyValuePair<string, string>("pageurl", pageUrl));
 		}
 
-		public async Task<_2CaptchaResult> SolveHCaptcha(string siteKey, string pageUrl, string proxy, ProxyType proxyType, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveHCaptcha(string siteKey, string pageUrl, string proxy, ProxyType proxyType, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("hcaptcha", 10, options,
 				new KeyValuePair<string, string>("sitekey", siteKey),
@@ -230,7 +215,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("proxytype", proxyType.GetExtension()));
 		}
 
-		public async Task<_2CaptchaResult> SolveGeeTest(string siteKey, string pageUrl, string challenge, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveGeeTest(string siteKey, string pageUrl, string challenge, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("geetest", 10, options,
 				new KeyValuePair<string, string>("gt", siteKey),
@@ -238,7 +223,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("pageurl", pageUrl));
 		}
 
-		public async Task<_2CaptchaResult> SolveClickCaptcha(Stream imageStream, FileType fileType, string task, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveClickCaptcha(Stream imageStream, FileType fileType, string task, params KeyValuePair<string, string>[] options)
 		{
 			var httpContent = new MultipartFormDataContent
 			{
@@ -250,7 +235,7 @@ namespace _2CaptchaAPI
 			return await Solve("post", 5, options, httpContent);
 		}
 
-		public async Task<_2CaptchaResult> SolveClickCaptcha(byte[] imageBytes, FileType fileType, string task, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveClickCaptcha(byte[] imageBytes, FileType fileType, string task, params KeyValuePair<string, string>[] options)
 		{
 			var imageBase64 = Convert.ToBase64String(imageBytes);
 			var imageData = EnsureValidData(imageBase64, fileType);
@@ -261,7 +246,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("textinstructions", task));
 		}
 
-		public async Task<_2CaptchaResult> SolveClickCaptcha(string imageBase64, FileType fileType, string task, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveClickCaptcha(string imageBase64, FileType fileType, string task, params KeyValuePair<string, string>[] options)
 		{
 			var imageData = EnsureValidData(imageBase64, fileType);
 
@@ -271,7 +256,7 @@ namespace _2CaptchaAPI
 				new KeyValuePair<string, string>("textinstructions", task));
 		}
 
-		public async Task<_2CaptchaResult> SolveRotateCaptcha(Stream[] imageStreams, FileType fileType, string rotateAngle, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveRotateCaptcha(Stream[] imageStreams, FileType fileType, string rotateAngle, params KeyValuePair<string, string>[] options)
 		{
 			var httpContent = new MultipartFormDataContent
 			{
@@ -286,7 +271,7 @@ namespace _2CaptchaAPI
 			return await Solve("rotatecaptcha", 5, options, httpContent);
 		}
 
-		public async Task<_2CaptchaResult> SolveFunCaptcha(string publicKey, string pageUrl, bool noJavaScript = false, string userAgent = null, string sUrl = null, Dictionary<string, string> data = null, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveFunCaptcha(string publicKey, string pageUrl, bool noJavaScript = false, string userAgent = null, string sUrl = null, Dictionary<string, string> data = null, params KeyValuePair<string, string>[] options)
 		{
 			var args = new List<KeyValuePair<string, string>>
 			{
@@ -305,11 +290,11 @@ namespace _2CaptchaAPI
 				args.AddRange(
 					data.Select(pair => new KeyValuePair<string, string>($"data[{pair.Key}]", pair.Value.ToString()))
 				);
-			
+
 			return await Solve("funcaptcha", 10, options, args.ToArray());
 		}
 
-		public async Task<_2CaptchaResult> SolveFunCaptcha(string publicKey, string pageUrl, string proxy, ProxyType proxyType, bool noJavaScript = false, string userAgent = null, string sUrl = null, Dictionary<string, string> data = null, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveFunCaptcha(string publicKey, string pageUrl, string proxy, ProxyType proxyType, bool noJavaScript = false, string userAgent = null, string sUrl = null, Dictionary<string, string> data = null, params KeyValuePair<string, string>[] options)
 		{
 			var args = new List<KeyValuePair<string, string>>
 			{
@@ -330,11 +315,11 @@ namespace _2CaptchaAPI
 				args.AddRange(
 					data.Select(pair => new KeyValuePair<string, string>($"data[{pair.Key}]", pair.Value.ToString()))
 				);
-			
+
 			return await Solve("funcaptcha", 10, options, args.ToArray());
 		}
 
-		public async Task<_2CaptchaResult> SolveKeyCaptcha(string userId, string sessionId, string webServerSign, string webServerSign2, string pageUrl, params KeyValuePair<string, string>[] options)
+		public async Task<Result> SolveKeyCaptcha(string userId, string sessionId, string webServerSign, string webServerSign2, string pageUrl, params KeyValuePair<string, string>[] options)
 		{
 			return await Solve("keycaptcha", 15, options,
 				new KeyValuePair<string, string>("s_s_c_user_id", userId),
--- 2Captcha/_2CaptchaResult.cs
+++ 2Captcha/_2CaptchaResult.cs
@@ -1,16 +1,97 @@
-﻿// ReSharper disable MemberCanBePrivate.Global
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
 
 namespace _2CaptchaAPI
 {
-	public struct _2CaptchaResult
-    {
-        public bool Success;
-        public string Response;
+	public partial class _2Captcha
+	{
+		[Serializable]
+		private struct ResultInternal
+		{
+			[JsonProperty("status")]
+			public int Status;
 
-        public _2CaptchaResult(bool success, string response)
-        {
-            Success = success;
-            Response = response;
-        }
-    }
+			[JsonProperty("request")]
+			public JToken Request;
+		}
+
+		public struct Result
+		{
+			public readonly bool Success;
+			public readonly JToken ResponseObject;
+
+			public Result(bool success, JToken responseObject)
+			{
+				Success = success;
+				ResponseObject = responseObject;
+
+				_responseJson = null;
+				_response = null;
+				_responseDouble = null;
+				_responseCoordinates = null;
+			}
+
+			private string _responseJson;
+			public string ResponseJson
+			{
+				get
+				{
+					if (_responseJson == null)
+						_responseJson = ResponseObject.ToString(Formatting.None);
+
+					return _responseJson;
+				}
+			}
+
+			private string _response;
+			public string Response
+			{
+				get
+				{
+					if (_response == null)
+						_response = ResponseObject.ToObject<string>();
+
+					return _response;
+				}
+			}
+
+			private double? _responseDouble;
+			public double ResponseDouble
+			{
+				get
+				{
+					if (_responseDouble == null)
+						_responseDouble = ResponseObject.ToObject<double>();
+
+					return _responseDouble.Value;
+				}
+			}
+
+			private Coordinates[] _responseCoordinates;
+			public Coordinates[] ResponseCoordinates
+			{
+				get
+				{
+					if (_responseCoordinates == null)
+						_responseCoordinates = ResponseObject.ToObject<Coordinates[]>();
+
+					return _responseCoordinates;
+				}
+			}
+		}
+
+		[Serializable]
+		public readonly struct Coordinates
+		{
+			[JsonProperty("x")]
+			public readonly int X;
+
+			[JsonProperty("y")]
+			public readonly int Y;
+		}
+	}
 }
